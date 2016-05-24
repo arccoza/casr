82f8763f2db6c91b483ec430a0f3a343a0bc352f
adrien <adrien@arccoza.com>
2016-05-24 16:59:33 +0900

    Add login page.

--- app/index.js
+++ app/index.js
@@ -120,6 +120,9 @@ app.set('view engine', 'jade')
 
 app.get('/', function (req, res) {
   res.render('index.jade')
+})
+  .get('/login', function (req, res) {
+  res.render('login.jade')
 });
 
 app.listen(8080);
